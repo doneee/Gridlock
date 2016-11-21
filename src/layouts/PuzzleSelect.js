8085c0b6a92da6f547b25c7840833609fb8da3be
Donny Swany <donnyswany@gmail.com>
2016-11-21 01:25:50 -0500

    forgot to save PuzzleSelect.js

---
+++ src/layouts/PuzzleSelect.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router';
+import FlatButton from 'material-ui/FlatButton';
+
+class PuzzleSelect extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <div className="layout-puzzle-select">
+
+      </div>
+    );
+  }
+}
+
+export default Main;
